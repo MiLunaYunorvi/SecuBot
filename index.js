ed20e121bd22f86a47d81ed51e1705cba6ffc617
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-08-13 17:30:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ var {ataques_opciones, ataques_llamada} = require('./ataques')
 const {initializeAndProcessChatbot} = require('./NLP');
 
 const config = {
-  webhookUrl: "https://29c2-179-6-14-226.ngrok-free.app",
+  webhookUrl: "18.217.250.67",
   token: "NDk5NjFiODItY2IwMS00ZDRlLWE5MDItMWVjY2JkMjU3NThhZjRlYWRlYTUtMjQ4_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f",
-  port: 7001
+  port: 80
 };
 
 // init framework
